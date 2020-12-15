53cd3881430a0ded3145f669933c9dc41def2c73
Sedon Attila <h985176@stud.u-szeged.hu>
2020-12-15 17:38:15 +0100

    The base FizzBuzz is ready

--- src/components/FizzBuzz.tsx
+++ src/components/FizzBuzz.tsx
@@ -1,5 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './FizzBuzz.css'
+const onetoHundred = Array.from(Array(101).keys());
+onetoHundred.shift();
 
 export const FizzBuzz = () => {
-    return null;
+    const [three, setThree] = useState("Fizz");
+    const [five, setFive] = useState("Buzz");
+    const writeFizzBuzz = (e: number): string => {
+        let s = "";
+        if(e % 3 === 0){
+            s += three;
+        }
+        if(e %5 === 0){
+            s += five;
+        }
+        return s || e.toString();
+    }
+
+    return <div className={"numberWrapper"}>
+        {onetoHundred.map(e => <span>{writeFizzBuzz(e)}</span>)}
+    </div>;
 }
