c3327e90df422e1500c2eb1229565e80a1b49114
Sedon Attila <h985176@stud.u-szeged.hu>
2020-12-15 18:23:09 +0100

    tests done

--- src/components/FizzBuzz.test.tsx
+++ src/components/FizzBuzz.test.tsx
@@ -7,14 +7,20 @@ describe("Test FizzBuzz Component", ()=> {
         test("should write Buzz for modulo 5", () => {
             render(<FizzBuzz />);
             expect(screen.getByTestId("5").textContent).toEqual("Buzz");
+            expect(screen.getByTestId("10").textContent).toEqual("Buzz");
+            expect(screen.getByTestId("20").textContent).toEqual("Buzz");
         });
         test("should write Fizz for modulo 3", () => {
             render(<FizzBuzz />);
             expect(screen.getByTestId("3").textContent).toEqual("Fizz");
+            expect(screen.getByTestId("6").textContent).toEqual("Fizz");
+            expect(screen.getByTestId("9").textContent).toEqual("Fizz");
         });
         test("should write FizzBuzz for modulo 15", () => {
             render(<FizzBuzz />);
             expect(screen.getByTestId("15").textContent).toEqual("FizzBuzz");
+            expect(screen.getByTestId("30").textContent).toEqual("FizzBuzz");
+            expect(screen.getByTestId("45").textContent).toEqual("FizzBuzz");
         });
     });
     describe("Check interactions", () => {
@@ -25,6 +31,14 @@ describe("Test FizzBuzz Component", ()=> {
             fireEvent.change(input, { target: { value: 'LOL' } });
             fireEvent.click(button);
             expect(screen.getByTestId("3").textContent).toEqual("LOL");
+        });
+        test('fiveInput should change value of Buzz', () => {
+            render(<FizzBuzz />);
+            const input = screen.getByTestId("fiveInput");
+            const button = screen.getByTestId("fiveButton");
+            fireEvent.change(input, { target: { value: 'KEK' } });
+            fireEvent.click(button);
+            expect(screen.getByTestId("5").textContent).toEqual("KEK");
         })
     });
 
