c07207b2449b99edab2f8acfb468b6d3f8ab9cfd
Sedon Attila <h985176@stud.u-szeged.hu>
2020-12-22 15:24:32 +0100

    Created a more general FizzBuzz element

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import {FizzBuzz} from "./components/FizzBuzz";
 
 function App() {
-  return (
-  <FizzBuzz />
-  );
+    return (
+        <>
+            <FizzBuzz/>
+            <FizzBuzz length={91} numberWordObject={new Map().set(4, "Foo").set(7, "Bar").set(3, "Baz")}/>
+        </>
+    );
 }
 
 export default App;
--- src/components/FizzBuzz.tsx
+++ src/components/FizzBuzz.tsx
@@ -1,38 +1,55 @@
-import React, {useState} from 'react';
+import React, {useState, useMemo} from 'react';
 import './FizzBuzz.css'
-const onetoHundred = Array.from(Array(101).keys());
-onetoHundred.shift();
 
-export const FizzBuzz = () => {
-    const [three, setThree] = useState("Fizz");
-    const [five, setFive] = useState("Buzz");
-    const [threeInput, setThreeInput] = useState("");
-    const [fiveInput, setFiveInput] = useState("");
+type FizzBuzzProps = {
+    length?: number;
+    numberWordObject?: Map<number, string>;
+}
 
-    const writeFizzBuzz = (e: number): string => {
-        if(e%3===0 && e%5===0){
-            return three+five;
-        } else if(e%3 === 0){
-            return three;
-        } else if (e%5===0){
-            return five;
-        } else {
-            return e.toString();
-        }
+export const FizzBuzz = ({length = 100, numberWordObject = new Map().set(3, "Fizz").set(5, "Buzz")}: FizzBuzzProps) => {
+    const [numWord, setNumWord] = useState(numberWordObject);
+    const [numWordInput, setNumWordInput] = useState(numberWordObject);
+
+    const onetoLength = useMemo(() =>{
+        const onetoLength = Array.from(Array(length+1).keys());
+        onetoLength.shift();
+        return onetoLength
+    }, [length])
+
+    const updateText = (num: number): void => {
+        setNumWord((state => new Map(numWord.set(num, numWordInput.get(num) ?? ""))));
+    }
+
+    const updateInput = (num: number, value: string): void => {
+        setNumWordInput((state => new Map(numWordInput.set(num, value))));
+    }
+
+    const writeFizzBuzz = (num: number): string => {
+        let str = "";
+        Array.from(numWord.keys()).forEach((elem) => {
+            console.log(elem);
+            if(num % elem === 0){
+                str += numberWordObject.get(elem);
+            }
+        })
+        return str || num.toString();
     }
 
     return (
         <div>
-            <div>
-                <input data-testid={"threeInput"} id={"threeInput"} onChange={(e)=> setThreeInput(e.target.value)} />
-                <button data-testid={"threeButton"} id={"threeButton"} onClick={(e)=> setThree(threeInput)}>Set Three</button>
-            </div>
-            <div>
-                <input data-testid={"fiveInput"} id={"fiveInput"} onChange={(e)=> setFiveInput(e.target.value)} />
-                <button data-testid={"fiveButton"} id={"fiveButton"} onClick={(e)=> setFive(fiveInput)}>Set Five</button>
-            </div>
+            {Array.from(numWord.keys()).map((key) => {
+                return (
+                    <div>
+                        <input data-testid={`${key}Input`} id={`${key}Input`}
+                               onChange={(e) => updateInput(key, e.target.value)}/>
+                        <button data-testid={`${key}Button`} id={`${key}Button`} onClick={() => updateText(key)}>Set
+                            &nbsp;{key}
+                        </button>
+                    </div>
+                )
+            })}
             <div className={"numberWrapper"}>
-                {onetoHundred.map(e => <span data-testid={e.toString()} id={e.toString()}>{writeFizzBuzz(e)}</span>)}
+                {onetoLength.map(e => <span data-testid={e.toString()} id={e.toString()}>{writeFizzBuzz(e)}</span>)}
             </div>
         </div>);
 }
